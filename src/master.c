b47397fd4751bef0a18d590e4bcc046718b16583
Claudiu Ghioc <claudiu.ghioc@gmail.com>
2012-11-17 23:24:28 +0200

    Separate work using MPI of file read
    Signed-off-by: Claudiu Ghioc <claudiu.ghioc@gmail.com>

--- src/master.c
+++ src/master.c
@@ -1,11 +1,12 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define CONFIG_FILE         "/home/claudiu/Desktop/2tema/config/config"
 
-int *reducers, mappers;
+int *reducers, mappers, in_file_size;
 char in_file[255], out_file[255];
 
 // Reads the configuration file and initiates data
@@ -28,16 +29,43 @@ static int init()
 
     fscanf(config, "%s", in_file);
     fscanf(config, "%s", out_file);
+    fclose(config);
+
+    FILE *fp = fopen(in_file, "r");
+    fseek(fp, 0L, SEEK_END);
+    in_file_size = ftell(fp);
+    printf("File size is %d\n", in_file_size);
+    fseek(fp, 0L, SEEK_SET);
+    fclose(fp);
+
     return 0;
 }
 
 // Execute the mapper code
 static int execute_mapper(MPI_Comm parentcomm)
 {
-    int rank, size;
+    int rank, size, block_size;
+    MPI_File mapper_file;
+    char buff[1000];
+    MPI_Status status;
+    MPI_Datatype arraytype;
+    MPI_Offset disp;
+
     MPI_Comm_rank(parentcomm, &rank);
     MPI_Comm_size(parentcomm, &size);
     printf("Mapper in parentcomm %d %d\n", rank, size);
+
+    // Open input file and set the view
+    MPI_File_open(MPI_COMM_WORLD, in_file, MPI_MODE_RDONLY, MPI_INFO_NULL, &mapper_file);
+
+    disp = rank * sizeof(char) * block_size;
+    MPI_Type_contiguous(block_size, MPI_CHAR, &arraytype);
+    MPI_Type_commit(&arraytype);
+    MPI_File_set_view(mapper_file, disp, MPI_CHAR, arraytype, "native", MPI_INFO_NULL);
+
+    MPI_File_read(mapper_file, buff, 1000, MPI_CHAR, &status);
+    MPI_File_close(&mapper_file);
+    printf("M %d read %s\n", rank, buff);
     return 0;
 }
 
@@ -47,6 +75,9 @@ static int execute_master()
     MPI_Comm intercomm;
     int rank, size;
 
+    if (init())
+        return 1;
+
     int *errcodes = (int *)malloc(mappers * sizeof(int));
     if (errcodes == NULL) {
         printf("Error on alocating err codes\n");
@@ -54,8 +85,16 @@ static int execute_master()
     }
 
     printf("P creates %d mappers\n", mappers);
+
+    //TODO send arguments to mapper processes
+    //Arguments: in_file, mappers
+    //Reducer per mapper will be received by send
+    char **args = (char **) malloc(3 * sizeof(char *));
+    args[0] = "Hello";
+    args[1] = "WORLD";
+
     // Create the mappers processes
-    MPI_Comm_spawn( "./master", MPI_ARGV_NULL, mappers, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &intercomm, errcodes );
+    MPI_Comm_spawn( "./master", args, mappers, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &intercomm, errcodes );
 
     MPI_Comm_rank(intercomm, &rank);
     MPI_Comm_size(intercomm, &size);
@@ -66,9 +105,10 @@ static int execute_master()
 int main(int argc, char **argv)
 {
     MPI_Comm parentcomm;
-
-    if (init())
-        return 1;
+    printf("Argc = %d\n", argc);
+    int i = 0;
+    for (i = 0; i < argc; i++)
+        printf("Arg %d: %s ", i, argv[i]);
 
     MPI_Init(&argc, &argv);
     MPI_Comm_get_parent(&parentcomm);
