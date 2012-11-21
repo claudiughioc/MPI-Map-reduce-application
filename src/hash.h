5261f993bdb9ba80d8fb4f6c8082b87dbd29cb2c
Claudiu Ghioc <claudiu.ghioc@gmail.com>
2012-11-21 23:14:59 +0200

    Let's switch to C++
    Signed-off-by: Claudiu Ghioc <claudiu.ghioc@gmail.com>

--- src/hash.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef _HASH_H
-#define _HASH_H
-
-unsigned int hash(const char *str, unsigned int hash_length);
-
-#endif
-
--- src/reducer.cpp
+++ src/reducer.cpp
@@ -1,15 +1,15 @@
 #include "mpi.h"
-#include "hashtable.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#define HASHTABLE_SIZE      3000
+#define HASHTABLE_SIZE      300000
 
-static void build_hashtable(char *buff, int size, hashtable *table)
+int debug;
+
+static void build_hashtable(char *buff, int size, int *table)
 {
-    
 }
 
 int main(int argc, char **argv)
@@ -19,9 +19,6 @@ int main(int argc, char **argv)
     int my_size;
     char *buff;
     MPI_Status status;
-
-    hashtable *table = create_hashtable(HASHTABLE_SIZE);
-    printf("Hashtable should be created\n");
 
     MPI_Init(&argc, &argv);
     MPI_Comm_get_parent(&parentcomm);
@@ -39,14 +36,12 @@ int main(int argc, char **argv)
     my_size = block_size / coworkers;
     if (rank + 1 == size)
         my_size += block_size % coworkers;
-    buff = (char *)malloc(my_size * sizeof(char) + 1);
+    buff = (char *)malloc((my_size + 1) * sizeof(char));
 
     // Now wait for the data from my mapper...
     MPI_Recv(buff, my_size, MPI_CHAR, 0, 1, parentcomm, &status);
     printf("Reducer %d of %d, received %d\n", rank, size, my_size);
 
-    //Build hashtable
-    build_hashtable(buff, my_size, table);
     
     MPI_Finalize();
     printf("Moare reducer %d din %d\n", rank, size);
